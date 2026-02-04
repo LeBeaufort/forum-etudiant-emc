3dea92b643f0bffac940f5dfce25ecd49aedb00e
LeBeaufort <135138712+LeBeaufort@users.noreply.github.com>
2026-02-04 21:21:36 +0100

    fixed logo, added forums and remove pycache

--- forum-etudiant/schema.sql
+++ forum-etudiant/schema.sql
@@ -34,4 +34,7 @@ CREATE TABLE forum (
 );
 
 -- creating default forums
+INSERT INTO forum (name) VALUES ("Forum collègien");
+INSERT INTO forum (name) VALUES ("Forum lycéen");
+INSERT INTO forum (name) VALUES ("Forum étude sup");
 INSERT INTO forum (name) VALUES ("Forum de test");
